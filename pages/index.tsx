90600d5a3a70dced428080a8bacb2e808b0f825e
baky2708 <flavioisaque123@gmail.com>
2022-10-28 09:25:31 -0300

    feat: test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import styles from '@/styles/Home.module.css';
+
+// Continuous Deployement
 
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>BakyLand</h1>
+      <h1 className={styles.h1}>BakyLand</h1>
+      <h2>Yaya</h2>
+      <h2>Yaya</h2>
+      <h2>Yaya</h2>
+      <h2>Yaya</h2>
+      <h2>Yaya</h2>
+      <h2>Yaya</h2>
+      <h2>Yaya</h2>
+      <h2>Yaya</h2>
       <h2>Yaya</h2>
       <h2>Test domain</h2>
     </div>
